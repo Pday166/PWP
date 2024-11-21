4a7e70c9ef723a04f89ae489294d87f1f09c1720
Patrick Day <patday30@gmail.com>
2024-11-20 20:05:12 -0700

    ready to turn in?

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
     content: ['index.html', './src/**/*.{js,css}',  "./node_modules/flowbite/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'smoke': 'url("/vid-spfx.jpg")',
+                'mixer': 'url("/mixer1.jpg")',
+                'editor': 'url("/vid-edit1.jpg")',
+                'coding': 'url("/coding.jpg")',
+            },
+            keyframes: {
+                    appear: {
+                        "0%": {
+                            opacity: "0",
+                        },
+                        "100%": {
+                            opacity: "1",
+                        },
+                    },
+                    animation: {
+                        appear: "appear 0.5s ease-in-out",
+                    }
+                }
+        },
     },
     plugins: [
         require('flowbite/plugin')
